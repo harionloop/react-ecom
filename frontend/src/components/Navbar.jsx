e146d8b19f490e4c3673640a3dfa3027d420701a
harionloop <harionloop@gmail.com>
2022-04-16 22:55:26 +0530

    Navbar done!

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,40 +1,84 @@
 import React from "react";
 import styled from "styled-components";
+import { Search, ShoppingCartSharp } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 
 const Container = styled.div`
   height: 60px;
-  background-color: lightpink;
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  color: darkblue;
+  border-bottom: 1px solid lightgray;
 `;
 
 const Left = styled.div`
   flex: 1;
+  align-items: center;
+  display: flex;
 `;
 const Language = styled.span`
   cursor: pointer;
   font-size: 14px;
 `;
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  margin-left: 25px;
+  border: solid darkblue 0.5px;
+`;
+const SearchInput = styled.input`
+  border: none;
+  padding: 5px;
+  outline: none;
+`;
 
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
+const MenuItem = styled.div`
+  font-size: 14px;
 
+  margin-left: 20px;
+`;
 export const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
           <Language>EN</Language>
+          <SearchContainer>
+            <SearchInput placeholder="Search..." />
+            <Search />
+          </SearchContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo>Deepak Enterprise</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>LOGIN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCartSharp />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
